13bd231b6c4d54de16fdd09944fa58b6f1011b06
babangsund <babangsund@gmail.com>
2019-10-09 21:01:48 +0200

    Remove cra boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
+// @flow
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section>
+      <header>
+        <section>Search</section>
       </header>
-    </div>
+      <main>
+        <section>List</section>
+      </main>
+    </section>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+// @flow
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+
+// project
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root: ?HTMLElement = document.getElementById('root');
+const element = <App />;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (root) {
+  render(element, root);
+}
+
+serviceWorker.register();
